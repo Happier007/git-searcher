47e59f710fbc2f9e082fb5e4f701f9e8cbb4ec4e
gennady <gennady.vaschenko@gmail.com>
2019-11-11 17:03:22 +0300

    "done user list and detail"

---
+++ src/app/modules/main/components/search-checked/search-checked.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-checked',
+  templateUrl: './search-checked.component.html',
+  styleUrls: ['./search-checked.component.scss']
+})
+export class SearchCheckedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
