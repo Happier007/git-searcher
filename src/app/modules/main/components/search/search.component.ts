d2bdceea1025e96be5f220d73514924d17b7089e
gennady <gennady.vaschenko@gmail.com>
2020-01-15 16:58:44 +0300

    version with chips

--- src/app/modules/main/components/search/search.component.ts
+++ src/app/modules/main/components/search/search.component.ts
@@ -1,5 +1,5 @@
 // ANGULAR
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -12,15 +12,18 @@ import { SelectionModel } from '@angular/cdk/collections';
 
 // RXJS
 import { Observable, Subject } from 'rxjs';
-import { pluck, takeUntil, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, pluck, takeUntil, tap } from 'rxjs/operators';
 
 // MODELS
-import { ISearch, IUserSearch } from '@models/search';
+import { IUserSearch } from '@models/search';
 import { IProfile } from '@models/profile';
 
 // MAIN
 import { GitService } from '@services/git.service';
 import { IQueryParams } from '@models/queryParams';
+import { MatTable } from '@angular/material/table';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import isEqual from 'lodash/isEqual';
 
 @Component({
     selector: 'app-search',
@@ -29,9 +32,9 @@ import { IQueryParams } from '@models/queryParams';
 })
 export class SearchComponent implements OnInit, OnDestroy {
     @ViewChild('usernameInput', {static: false}) usernameInput: ElementRef<HTMLInputElement>;
+    @ViewChild('table', {static: false}) table: MatTable<Element>;
     private searchForm: FormGroup = this.buildForm();
     private destroy$: Subject<void> = new Subject<void>();
-    public users$: Observable<IUserSearch[]> = new Observable<IUserSearch[]>();
     public detailUser: IProfile;
     public checkedUsers: IProfile[] = [];
     public queryParams: IQueryParams;
@@ -39,6 +42,9 @@ export class SearchComponent implements OnInit, OnDestroy {
     public displayedColumns: string[] = ['select', 'ava', 'login', 'score', 'url'];
     public selection = new SelectionModel<IUserSearch>(true, []);
     public loading = false;
+
+    public chips: IUserSearch[] = [];
+    public usersChips$: Observable<IUserSearch[]> = new Observable<IUserSearch[]>();
 
     constructor(private router: Router,
                 private route: ActivatedRoute,
@@ -48,10 +54,13 @@ export class SearchComponent implements OnInit, OnDestroy {
 
     ngOnInit(): void {
         this.queryParams = this.gitService.getRouteParams();
-        if (this.queryParams.q) {
-            this.loadUsers(this.queryParams.q);
-            this.searchForm.patchValue({username: this.queryParams.q});
-        }
+
+        this.searchForm.get('username').valueChanges
+            .pipe(
+                debounceTime(250),
+                distinctUntilChanged(isEqual),
+                takeUntil(this.destroy$),
+            ).subscribe((users) => this.loadUsers(users));
     }
 
     ngOnDestroy(): void {
@@ -69,54 +78,25 @@ export class SearchComponent implements OnInit, OnDestroy {
     private submitForm(): void {
         if (this.searchForm.valid) {
             const searchName = this.searchForm.value.username;
-            this.updateQueryParams(searchName, null, 0, 10);
             this.loadUsers(searchName);
             this.detailUser = null;
             this.checkedUsers = [];
         }
     }
 
-    private updateQueryParams(q: string, id: number = null, pageIndex?: number, pageSize?: number): void {
-        this.queryParams.q = q;
-        this.queryParams.id = id;
-        if (pageIndex >= 0) {
-            this.queryParams.page = pageIndex;
-        }
-        if (pageSize > 0) {
-            this.queryParams.pageSize = pageSize;
-        }
-        this.router.navigate([], {
-            queryParams: {
-                q: this.queryParams.q,
-                id: this.queryParams.id,
-                page: this.queryParams.page,
-                per_page: this.queryParams.pageSize,
-            }
-        });
-    }
-
     private loadUsers(username: string): void {
         this.loading = true;
-        this.users$ = this.gitService.searchUsers(username, this.queryParams.page, this.queryParams.pageSize)
+        this.usersChips$ = this.gitService.searchUsers(username, this.queryParams.page, this.queryParams.pageSize)
             .pipe(
-                tap((res: ISearch) => {
-                    this.countUsers = res.total_count;
-                }),
                 pluck('items'),
-                tap((users: IUserSearch[]) => {
-                    if (this.queryParams.id) {
-                        const user = users.find(item => item.id === this.queryParams.id);
-                        this.selectUser(user.login, 'detail');
-                    }
-                    this.loading = false;
-                })
+                tap(() => this.loading = false)
             );
 
     }
 
+
     public selectRow(row): void {
         this.selectUser(row.login, 'detail');
-        this.updateQueryParams(this.queryParams.q, row.id);
     }
 
     public paginatorEvent(event: PageEvent): PageEvent {
@@ -127,7 +107,6 @@ export class SearchComponent implements OnInit, OnDestroy {
             this.detailUser = null;
         }
         if (this.queryParams.q) {
-            this.updateQueryParams(this.queryParams.q, this.queryParams.id);
             this.loadUsers(this.queryParams.q);
         }
         return event;
@@ -189,4 +168,20 @@ export class SearchComponent implements OnInit, OnDestroy {
     private removeUser(id: number): void {
         this.checkedUsers = this.checkedUsers.filter(item => item.id !== id);
     }
+
+    public removeChips(user: IUserSearch): void {
+        const index = this.chips.indexOf(user);
+        if (index >= 0) {
+            this.chips.splice(index, 1);
+            this.table.renderRows();
+        }
+    }
+
+    public selectedChips(event: MatAutocompleteSelectedEvent): void {
+        this.chips.push(event.option.value);
+        this.countUsers = this.chips.length;
+        this.usernameInput.nativeElement.value = '';
+        this.table.renderRows();
+    }
 }
+
